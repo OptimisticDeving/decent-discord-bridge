838a1a6663c0626b870d496807b65f6540e04b80
OptimisticDeving <173472493+OptimisticDeving@users.noreply.github.com>
2024-06-28 02:33:58 +0000

    "Fix" gradle buildscript
    Fixes #3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,12 @@
+import com.google.gson.Gson
+import com.google.gson.JsonArray
+import com.google.gson.JsonObject
+import java.io.BufferedOutputStream
+import java.nio.file.Files
+import java.util.jar.JarInputStream
+import java.util.jar.JarOutputStream
+import java.util.zip.ZipEntry
+
 plugins {
     kotlin("jvm") version "2.0.0"
     id("fabric-loom") version "1.7-SNAPSHOT"
@@ -10,28 +19,33 @@ loom {
 group = "dev.optimistic"
 version = "1.1.0"
 
-val shaded: Configuration by configurations.creating
+val jij: Configuration by configurations.creating
 
 dependencies {
     minecraft("com.mojang:minecraft:1.20.1")
     mappings("net.fabricmc:yarn:1.20.1+build.10:v2")
     modImplementation("net.fabricmc:fabric-loader:0.15.11")
-    modImplementation("net.fabricmc:fabric-language-kotlin:1.11.0+kotlin.2.0.0")
 
     // config
-    shaded("org.spongepowered:configurate-core:4.1.2")
-    shaded("org.spongepowered:configurate-extra-kotlin:4.1.2")
-    shaded("me.lucko.configurate:configurate-toml:4.1")
+    jij("org.spongepowered:configurate-core:4.1.2")
+    jij("org.spongepowered:configurate-extra-kotlin:4.1.2")
+    jij("me.lucko.configurate:configurate-toml:4.1")
 
     // discord
-    shaded("net.dv8tion:JDA:5.0.0-beta.24")
-    shaded("club.minnced:jda-ktx:0.11.0-beta.20")
-    shaded("club.minnced:discord-webhooks:0.8.4")
+    jij("net.dv8tion:JDA:5.0.0-beta.24") {
+        exclude(module = "opus-java")
+    }
+    jij("club.minnced:jda-ktx:0.11.0-beta.20")
+    jij("club.minnced:discord-webhooks:0.8.4")
 }
 
 configurations {
     compileClasspath {
-        extendsFrom(shaded)
+        extendsFrom(jij)
+    }
+
+    runtimeClasspath {
+        extendsFrom(jij)
     }
 }
 
@@ -57,17 +71,57 @@ tasks {
     }
 
     processResources {
-        filesMatching("fabric.mod.json") {
-            expand("version" to project.version)
+        dependsOn(jij)
+
+        jij.forEach { file ->
+            val inputStream = JarInputStream(file.inputStream())
+            val path = Files.createTempFile("giggle", "gaggle")
+            val fileOutputStream = BufferedOutputStream(Files.newOutputStream(path))
+            val jarOutputStream = JarOutputStream(fileOutputStream)
+            var entry = inputStream.nextJarEntry
+
+            while (entry != null) {
+                jarOutputStream.putNextEntry(entry as ZipEntry)
+                val entryBytes = inputStream.readAllBytes()
+                jarOutputStream.write(entryBytes, 0, entryBytes.size)
+                jarOutputStream.closeEntry()
+                inputStream.closeEntry()
+                entry = inputStream.nextJarEntry
+            }
+
+            jarOutputStream.putNextEntry(ZipEntry("fabric.mod.json"))
+
+            val fabricModJson = JsonObject()
+            fabricModJson.addProperty("schemaVersion", 1)
+            fabricModJson.addProperty("id", file.nameWithoutExtension.lowercase().replace(Regex("[^a-z0-9_-]"), "_"))
+            fabricModJson.addProperty("version", "0.0.0")
+            fabricModJson.addProperty("name", file.nameWithoutExtension)
+
+            val jsonBytes = Gson().toJson(fabricModJson).encodeToByteArray()
+            jarOutputStream.write(jsonBytes, 0, jsonBytes.size)
+            jarOutputStream.closeEntry()
+            jarOutputStream.flush()
+            jarOutputStream.close()
+            fileOutputStream.flush()
+            fileOutputStream.close()
+
+            from(path) {
+                rename { "META-INF/jars/${file.name}" }
+            }
         }
 
-        dependsOn(shaded)
-
-        shaded.forEach {
-            from(zipTree(it)) {
-                duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-                exclude("META-INF/*.RSA", "META-INF/*.SF", "META-INF/*.DSA")
+        filesMatching("fabric.mod.json") {
+            val jarsJson = JsonArray()
+            jij.forEach {
+                val jsonObject = JsonObject()
+                jsonObject.addProperty("file", "META-INF/jars/${it.name}")
+                jarsJson.add(jsonObject)
             }
+
+            expand(
+                "version" to project.version,
+                "jars" to jarsJson.toString()
+            )
         }
     }
 }

0b050a90e30ee12d9f5eb34145522c09c4a38614
OptimisticDeving <173472493+OptimisticDeving@users.noreply.github.com>
2024-06-29 06:03:54 +0000

    Add clarifying comment to null check

--- src/main/java/dev/optimistic/decentdiscordbridge/mixin/GameProfileMixin.java
+++ src/main/java/dev/optimistic/decentdiscordbridge/mixin/GameProfileMixin.java
@@ -24,6 +24,15 @@ public abstract class GameProfileMixin implements CachedAvatarUrlDuck {
     @Override
     public String getAvatarUrl() {
         if (this.avatarUrl == null) {
+            /*
+             this might technically be possible with mods that create fake players *improperly*,
+             as we mixin to PlayerManager's createPlayer to call the init func on the player's GameProfile,
+             so mods that don't call that function could cause the field to never be initialized, thus null
+
+             although JDA does allow for null avatar urls, we'd rather users not get confused between player
+             chat and system chat, as a null avatar url (i.e. the webhook's configured avatar)
+             is what is used by system chat messages
+             */
             DecentDiscordBridge.Companion.getLogger()
                     .error("avatar url SHOULD be initialized before this call, but it wasn't, so initializing now!!!");
             this.calculateAvatarUrl();
